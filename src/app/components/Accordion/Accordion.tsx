be14689e072835d40fb4b55fd951185437a0b36e
Alexandre Gros <xelaflash@gmail.com>
2023-06-01 16:29:54 -0400

    accordion gsap effect

---
+++ src/app/components/Accordion/Accordion.tsx
@@ -0,0 +1,78 @@
+import { gsap } from 'gsap';
+import style from './Accordion.module.scss';
+import { useRef, useState } from 'react';
+
+interface ItemData {
+  backgroundImage: string;
+}
+
+export default function Accordion() {
+  const [clickedItems, setClickedItems] = useState<boolean[]>([]);
+  const itemRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  const expand = (index: number) => {
+    setClickedItems((prevClickedItems) => {
+      const updatedClickedItems = [...prevClickedItems];
+      updatedClickedItems[index] = !updatedClickedItems[index];
+      return updatedClickedItems;
+    });
+
+    const items = itemRefs.current;
+    const item = items[index];
+
+    gsap.to(item, {
+      width: clickedItems[index] ? '15vw' : '42vw',
+      duration: 2,
+      ease: 'elastic(1, .7)',
+    });
+
+    items.forEach((it, i) => {
+      if (i !== index) {
+        gsap.to(it, {
+          width: clickedItems[index] ? '15vw' : '8vw',
+          duration: 2.5,
+          ease: 'elastic(1, .4)',
+        });
+      }
+    });
+  };
+
+  const itemsData: ItemData[] = [
+    {
+      backgroundImage:
+        'https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+      backgroundImage:
+        'https://images.unsplash.com/photo-1574879948818-1cfda7aa5b1a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80',
+    },
+    {
+      backgroundImage:
+        'https://images.unsplash.com/photo-1558383409-ab7ef8db3e01?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1193&q=80',
+    },
+    {
+      backgroundImage:
+        'https://images.unsplash.com/photo-1547059470-3b0c7cd958a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80',
+    },
+    {
+      backgroundImage:
+        'https://images.unsplash.com/photo-1558383738-8e44bbf6b093?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    },
+  ];
+
+  return (
+    <div className={style.group}>
+      {itemsData.map((itemData, index) => (
+        <div
+          className={`${style.item} ${
+            clickedItems[index] ? style.clicked : ''
+          }`}
+          style={{ backgroundImage: `url(${itemData.backgroundImage})` }}
+          key={index}
+          ref={(element) => (itemRefs.current[index] = element)}
+          onClick={() => expand(index)}
+        ></div>
+      ))}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,41 @@
 'use client';
 
+// next/react
+import Link from 'next/link';
+import Image from 'next/image';
+
+// Assets
+import logo from '../../public/logo.png';
+
+// components
 import GlowButton from './components/GlowButton/GlowButton';
+import Accordion from './components/Accordion/Accordion';
 
 export default function Home() {
   return (
-    <main className=" min-h-screen p-24 ">
-      <div className="font-mono  mb-16">
-        <h1 className=" text-6xl font-bold text-white drop-shadow-lg shadow-blue-500/50">My UI Kit</h1>
+    <main className="min-h-screen p-24 ">
+      <div className="m-auto w-fit flex gap-10 items-center font-mono mb-16 pb-2 border-b-2 border-fuchsia-900 ">
+        <Image src={logo} alt="Picture of the author" width={80} height={80} />
+        <h1 className="text-6xl font-bold text-white drop-shadow-lg shadow-blue-500/50">
+          UI Kit
+        </h1>
       </div>
 
-      <div className=" flex place-items-center">
+      <div className="flex place-items-center">
+        <Link
+          href="http://localhost:6006/"
+          className="font-mono  mb-16 text-xl text-white underline underline-offset-4 hover:no-underline"
+          target="_blank"
+        >
+          🚧 View StoryBook 🚧
+        </Link>
+      </div>
+      <div>
+        <h2 className="my-16 font-mono underline underline-offset-2 m-auto text-center text-4xl">
+          Components
+        </h2>
         <GlowButton />
+        <Accordion />
       </div>
     </main>
   );
